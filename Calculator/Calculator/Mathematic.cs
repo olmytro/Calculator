e2d3541428d4e904913536ba9f943835f54cf05d
olmytro <ironbro24@gmail.com>
2022-10-30 14:57:19 +0300

    class done
    add class with mathematic operation.

--- Calculator/Calculator/Mathematic.cs
+++ Calculator/Calculator/Mathematic.cs
@@ -9,6 +9,7 @@ namespace Calculator
         public int num1;
         public int num2;
         public string symbol;
+        public int result;
 
         public Mathematic(int aNum1, int aNum2, string aSymbol)
         {
@@ -22,10 +23,24 @@ namespace Calculator
             switch (symbol)
             {
                 case "+":
-                    Console.WriteLine("summ");
+                    Console.WriteLine("Summ");
+                    result = num1 + num2;
+                    Console.WriteLine("the result is: " + num1 + " + " + num2 + " = " + result );
                     break;
                 case "-":
-                    Console.WriteLine("substract");
+                    Console.WriteLine("Subtract");
+                    result = num1 - num2;
+                    Console.WriteLine("the result is: " + num1 + " - " + num2 + " = " + result);
+                    break;
+                case "*":
+                    Console.WriteLine("Multiply");
+                    result = num1 * num2;
+                    Console.WriteLine("the result is: " + num1 + " * " + num2 + " = " + result);
+                    break;
+                case "/":
+                    Console.WriteLine("divide");
+                    result = num1 / num2;
+                    Console.WriteLine("the result is: " + num1 + " / " + num2 + " = " + result);
                     break;
             }
         }
--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -6,78 +6,24 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.Write("write first number: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("write second numer: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("write operation: ");
-            string symbol = Console.ReadLine();
-
-            Console.WriteLine("This is the calculator");
-            Mathematic math = new Mathematic(num1, num2, symbol);
-
-
-            math.Back();
-
-            /*
-            int num1 = 0;
-            int num2 = 0;
-            int result = 0;
-
-            Console.Write("write first number: ");
-            num1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("write second numer: ");
-            num2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("write operation: ");
-            string symbol = Console.ReadLine();
-
-            
-            
             do
-            {   
+            {
                 Console.Write("write first number: ");
-                num1 = Convert.ToInt32(Console.ReadLine());
+                int num1 = Convert.ToInt32(Console.ReadLine());
 
                 Console.Write("write second numer: ");
-                num2 = Convert.ToInt32(Console.ReadLine());
+                int num2 = Convert.ToInt32(Console.ReadLine());
 
                 Console.Write("Enter the operator (+, -, *, /): ");
+                string symbol = Console.ReadLine();
 
-                switch (Console.ReadLine())
-                {
-                    case "+":
-                        result = num1 + num2;
-                        Console.WriteLine("{0} + {1} = {2}", num1, num2, (num1 + num2));
-                        //Console.WriteLine($"Your result: {num1} + {num2} =" + result);
-                        break;
-                    case "-":
-                        result = num1 - num2;
-                        Console.WriteLine($"Your result: {num1} - {num2} =" + result);
-                        break;
-                    case "*":
-                        result = num1 * num2;
-                        Console.WriteLine($"Your result: {num1} * {num2} =" + result);
-                        break;
-                    case "/":
-                        if (num1 == 0)
-                        {
-                            Console.WriteLine("Can't divide by zero");
-                        }
-                        else
-                        Console.WriteLine($"Your result: {num1} / {num2} =" + result);
-                        break;
-                    default:
-                        Console.WriteLine("invalid operation");
-                        break;
-                }
+                Mathematic math = new Mathematic(num1, num2, symbol);
+
+                math.Back();
+
                 Console.WriteLine("Continue?: Y or N:");
             }
             while (Console.ReadLine().ToUpper() == "Y");
-            */
 
             Console.ReadKey();
         }
